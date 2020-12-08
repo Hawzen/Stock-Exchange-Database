3fb709fb4ad4db353941204f16184dcbd7787f0a
AbdulrahmanSaleh <AbdulrahmanAlshawi@D7.noreply.github.com>
2020-12-08 03:54:19 +0300

    Did what i could do );

--- Contents/GUIPY/gui.py
+++ Contents/GUIPY/gui.py
@@ -57,12 +57,17 @@ lblfrstrow = tk.Label(root, text ="Stock Market Database")
 lblfrstrow.place(x = w/2, y = h/10) 
 
 
-tables = ["mark", "inv", "bank"]
-names = ["Stock_Market", "Investor", "Bank"]
+tables = ["mark", "inv", "bank" , "stock" , "company" , "ceo" , "secures" , "owns"]
+names = ["Stock_Market", "Investor", "Bank" , "Stock" , "Company" , "CEO" , "Secures" , "Owns"]
 tableToName = {t:n for t, n in zip(tables, names)}
 attributeList = [["Market_Name", "Currency", "Market_Cap_in_trillion", "Number_Of_Stock_Symbols"],
                 ["IBAN_number", "National_ID", "Sex", "NetWorth", "First_Name", "Middle_Name", "Last_Name"],
-                ["Commercial_Registration_Number","Name","Number_Of_Branches"]]
+                ["Commercial_Registration_Number","Name","Number_Of_Branches"],
+                ["Stock_Symbol" , "Number_Of_Public_Stocks_in_thousand" , "Stock_Price" , "Market_Name" , "Commercial_Registration_Number"],
+                ["Commercial_Registration_Number" , "Number_Of_Directors" , "Company_Equity_in_thousand" , "Company_Name"],
+                ["Name" , "Sex" , "Age" , "Commercial_Registration_Number"],
+                ["Stock_Symbol" , "IBAN_number" , "Commercial_Registration_Number"],
+                ["Stock_Symbol" , "IBAN_number" , "Number_Of_Shares"]]
 
 commands = {}
 texts = {}
@@ -70,9 +75,24 @@ labels = {}
 for table in tables:
     commands[table] = {}
     texts[table] = StringVar()
+#Add Comaands
 commands["mark"]["insert"] = """insert into Stock_Market(Market_Name,Currency,Market_Cap_in_trillion,Number_Of_Stock_Symbols) values ("{}", "{}",  {},  {});"""
 commands["inv"]["insert"] = """insert into Investor(IBAN_number, National_ID, Sex, NetWorth, First_Name, Middle_Name, Last_Name) values ('{}','{}','{}',{},'{}','{}','{}');"""
 commands["bank"]["insert"] = """insert into Bank(Commercial_Registration_Number, Name, Number_Of_Branches) values ('{}','{}', {});"""
+commands["stock"]["insert"] = """insert into Stock(Stock_Symbol, Number_Of_Public_Stocks_in_thousand, Stock_Price, Market_Name, Commercial_Registration_Number) values ('{}','{}', {});"""
+commands["company"]["insert"] = """insert into Company(Commercial_Registration_Number,Number_Of_Directors,Company_Equity_in_thousand,Company_Name) values ('{}','{}', {});"""
+commands["ceo"]["insert"] = """insert into CEO(Name,Sex,Age,Commercial_Registration_Number) values ('{}','{}', {});"""
+commands["secures"]["insert"] = """insert into Secures(Stock_Symbol,IBAN_number,Commercial_Registration_Number)  values ('{}','{}', {});"""
+commands["owns"]["insert"] = """insert into Owns(Stock_Symbol,IBAN_number,Number_Of_Shares) values ('{}','{}', {});"""
+#Delete commands
+commands["mark"]["delete"] = """delete from Stock_Market where Market_Name='{}';"""
+commands["inv"]["delete"] = """delete from Investor where IBAN_number='{}';"""
+commands["bank"]["delete"] = """delete from Bank where Commercial_Registration_Number='{}';"""
+commands["stock"]["delete"] = """delete from Stock where Stock_Symbol='{}';"""
+commands["company"]["delete"] = """delete from Company where Commercial_Registration_Number='{}';"""
+commands["ceo"]["delete"] = """delete from CEO where Name='{}' AND Commercial_Registration_Number='{}';"""
+commands["secures"]["delete"] = """delete from Secures where Stock_Symbol='{}' AND IBAN_number='{}' AND Commercial_Registration_Number='{}';"""
+commands["owns"]["delete"] = """delete from Owns where Stock_Symbol='{}' AND IBAN_number='{}';"""
 
 # Setup Add and Delete
 frames = {}
@@ -91,9 +111,9 @@ for attributes, name, table in zip(attributeList, names, tables):
     add = lambda q=commands[table]["insert"], vals=fields[table].values(), t=table: getterToDB(q, vals, t)
     Button(frames[table], text="Add", command=add).pack()
 
-    # Deleting
+    # Deleting Note: i think it has to have it own loop or function cause of diffrences in vals so tell me when you edit it
     # delete = lambda q=commands[table]["delete"], vals=fields[table].values(), t=table: getterToDB(q, vals, t)
-    # Button(frames[table], text="Delete", command=delete).pack()
+     #Button(frames[table], text="Delete", command=delete).pack()
 
     # Text
     labels[table] = Label(frames[table], textvariable=texts[table])
